5e45bbd9eef0ea2d1aea99fb256214eee132ca62
Charlie Money <cmoney5748@gmail.com>
2022-02-26 00:08:44 -0500

    added code for models

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,35 @@
 const { Schema, model } = require('mongoose');
+const Reaction = require('./reaction')
+
+const thoughtSchema = new Schema(
+    {
+      reactions: [Reaction],
+      thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+  thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  })
+  
+  
+  module.exports = thoughtSchema;
--- models/user.js
+++ models/user.js
@@ -1,1 +1,41 @@
 const { Schema, model } = require('mongoose');
+
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+      },
+      email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+  userSchema.virtual('friendCount').get(function() {
+    return this.reactions.length;
+  })
+  
+  module.exports = reactionSchema;
